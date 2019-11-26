f82f6bc3df0527cac9b62aa001ce9dad4a708254
Chris Groves <chris_groves87@hotmail.co.uk>
2019-11-26 16:54:47 +0000

    Thermostat starts at 20 degrees
    -create Thermostat function 
    
    - use prototype to 'bind' getCurrentTemperature function to Thermostat

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('Thermostat', function() {
+
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('starts at 20 degrees', function() {
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+
+});
