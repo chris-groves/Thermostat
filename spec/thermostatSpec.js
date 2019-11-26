1fa406b47010dfa3953b53ee77bfc6acdf370b8b
Chris Groves <chris_groves87@hotmail.co.uk>
2019-11-26 18:15:36 +0000

    Minimum temperature of 10 degrees
    - use minimum temperature constant

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -17,4 +17,17 @@ describe('Thermostat', function() {
     expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
 
+  it('can decrease in temperature', function() {
+    thermostat.Down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+
+  it('has a minimum temperature of 10 degress', function() {
+    for (var i = 0; i < 10; i++) {
+      thermostat.Down();
+    }
+    thermostat.Down();
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,6 +2,7 @@
 
 function Thermostat() {
   this.temperature = 20;
+  this.MINIMUM_TEMPERATURE = 10;
 }
 
 Thermostat.prototype.getCurrentTemperature = function() {
@@ -10,4 +11,11 @@ Thermostat.prototype.getCurrentTemperature = function() {
 
 Thermostat.prototype.Up = function() {
   this.temperature += 1;
+};
+
+Thermostat.prototype.Down = function() {
+  if (this.temperature === this.MINIMUM_TEMPERATURE) {
+    return;
+  }
+  this.temperature -= 1;
 };
